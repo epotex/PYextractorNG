1ed18054b910889ae058a2d51d421460dab559bc
epotex <epotexg@gmail.com>
2016-10-19 17:03:47 -0700

    + Add support for tmdb search api

--- PYextractorNG.py
+++ PYextractorNG.py
@@ -6,18 +6,25 @@ except ImportError: #if module extption raised  exit
     exit()
 import logging
 from pytvdbapi import api
+import tmdbsimple as tmdb
 
-moviedb_api_key = "a8b9f96dde091408a03cb4c78477bd14"
+###Variables ####
+moviedb_api_key = "7fd4c0a5340d510450ca53c37925e5ba"
 tvdb_api_key = "B43FF87DE395DF56"
 log_file = "PYextractorNG.log"
 logging.basicConfig(filename=log_file,level=logging.DEBUG, format='%(asctime)s %(message)s')
-###Variables ####
 root_folder = "/Users/moshe_edri/Google Drive/tmp" # Config the root folder to start the scan from, Example:  "C:\" or "/tmp/
 extentions = ['rar','zip'] # the extentions to look for, you can add more (7z, tar,gz, etc)
 archive_dict = {}
 general_extract_path = "/general/"# Config the path to a general folder for extraction , Example:  "C:\" or "/tmp/
 movie_extract_path = "/movie/"# Config the path to the movies folder, Example:  "C:\" or "/tmp/
 tv_show_extract_path = "/tv_shows" #Config the path to the tv shows folder, Example:  "C:\" or "/tmp/
+
+
+
+
+
+
 def spider(folder):
     logging.info("Starting the scan in %s " % (folder))
     for folderName, subfolders, filenames in os.walk(folder):
@@ -36,17 +43,22 @@ def spider(folder):
     logging.info("FINISHED Extracting files")
 
 def extractor(file, extract_path):
-
     logging.info("Extracting %s to %s" % (file, extract_path))
-
     #extractor("test.zip").extractall("/")
 
-def themoviedb_check(movie):
-    pass
+
 def tvdb_search(tvshow):
     db = api.TVDB(tvdb_api_key)
     result = db.search(tvshow, 'en')
     if len(result) == 1:
+        return True
+    else:
+        return False
+def themoviedb_check(movie):
+    tmdb.API_KEY = moviedb_api_key
+    search = tmdb.Search()
+    response = search.movie(query=movie)
+    if response['total_results'] > 0:
         return True
     else:
         return False
@@ -67,3 +79,7 @@ def categorise(filename):
         logging.info("Couldn't categorise %s, the archive will be extracted to the general extract folder, adding it to the queue.  " % (filename))
 
 spider(root_folder)
+
+
+
+
